4c2ee3714bef8768a013a49d3ba4990420d6b871
carson mulligan <carsonmulligan45@gmail.com>
2022-08-18 10:44:10 -0500

    madmen

--- config/routes/madmin.rb
+++ config/routes/madmin.rb
@@ -1,16 +1,17 @@
 # Below are the routes for madmin
 namespace :madmin do
+  resources :services
   namespace :active_storage do
     resources :variant_records
+  end
+  resources :users
+  namespace :active_storage do
+    resources :blobs
   end
   namespace :active_storage do
     resources :attachments
   end
-  resources :users
   resources :announcements
-  resources :services
-  namespace :active_storage do
-    resources :blobs
-  end
+  resources :notifications
   root to: "dashboard#show"
 end
