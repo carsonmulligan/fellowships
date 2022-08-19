f7ad5d32e699550aa0b4041ff5fbc796db923090
carson mulligan <carsonmulligan45@gmail.com>
2022-08-19 10:10:50 -0500

    test;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_theme
 
   protected
 
@@ -12,4 +13,14 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
     end
+
+
+  def set_theme
+    if params[:theme].present?
+      theme = params[:theme].to_sym
+      # session[:theme] = theme
+      cookies[:theme] = theme
+      redirect_to(request.referer || root_path)
+    end
+  end
 end
