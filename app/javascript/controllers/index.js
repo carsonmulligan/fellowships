c365520eb125dcc234dae708279e5125608ebbdc
carson mulligan <carsonmulligan45@gmail.com>
2022-08-18 21:06:14 -0500

    h

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,4 @@
-import { application } from "./application"
+import { application } from "../application"
 import controllers from './**/*_controller.js'
 controllers.forEach((controller) => {
   application.register(controller.name, controller.module.default)
