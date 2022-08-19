cdc7712229266a53b7e42eafb86f272f662f401e
carson mulligan <carsonmulligan45@gmail.com>
2022-08-18 20:54:26 -0500

    m

--- app/controllers/scholarships_controller.rb
+++ app/controllers/scholarships_controller.rb
@@ -6,14 +6,27 @@ class ScholarshipsController < ApplicationController
   end
 
   def index
+    if params[:query].present?
+      sql_query = 'name ILIKE :query OR description ILIKE :query'
+      @scholarships = Scholarship.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @scholarships = Scholarship.all
+    end
   end
 
   def show
+    @scholarship = Scholarship.find(params[:id])
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def scholarship_params
+    params.require(:scholarship).permit(:name, :description, :url, :due_date, :value)
   end
 end
