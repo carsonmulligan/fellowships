1dfedba4873d3ac14aaa84fb6c06680d34e2f04f
carson mulligan <carsonmulligan45@gmail.com>
2022-08-22 06:31:32 -0500

    remove send welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  after_create :send_welcome_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
